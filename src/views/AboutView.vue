d0186967d6eaf4ce3818a234c4bb72dc18c5902a
black-code1 <legrandmahamat1@gmail.com>
2022-03-19 14:40:47 +0100

    Router Links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, delectus
+      totam enim sint omnis possimus corporis a ducimus exercitationem eos
+      mollitia distinctio veniam optio sunt debitis magnam facere? Veniam porro
+      voluptatum perspiciatis harum aut minus at itaque consectetur vitae
+      veritatis. Voluptatibus amet eaque dicta nisi quis ullam officiis, odio,
+      alias veritatis illum saepe quidem. Libero sapiente cumque eius debitis
+      eveniet adipisci ut! Excepturi sequi, commodi sapiente ab, quos vitae quas
+      et quod tempore accusantium illo ea voluptatem est accusamus. Facilis at
+      sapiente impedit saepe quo minima autem recusandae incidunt consequuntur
+      aspernatur, sequi quae hic beatae eaque fugit aliquid neque excepturi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, delectus
+      totam enim sint omnis possimus corporis a ducimus exercitationem eos
+      mollitia distinctio veniam optio sunt debitis magnam facere? Veniam porro
+      voluptatum perspiciatis harum aut minus at itaque consectetur vitae
+      veritatis. Voluptatibus amet eaque dicta nisi quis ullam officiis, odio,
+      alias veritatis illum saepe quidem. Libero sapiente cumque eius debitis
+      eveniet adipisci ut! Excepturi sequi, commodi sapiente ab, quos vitae quas
+      et quod tempore accusantium illo ea voluptatem est accusamus. Facilis at
+      sapiente impedit saepe quo minima autem recusandae incidunt consequuntur
+      aspernatur, sequi quae hic beatae eaque fugit aliquid neque excepturi.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, delectus
+      totam enim sint omnis possimus corporis a ducimus exercitationem eos
+      mollitia distinctio veniam optio sunt debitis magnam facere? Veniam porro
+      voluptatum perspiciatis harum aut minus at itaque consectetur vitae
+      veritatis. Voluptatibus amet eaque dicta nisi quis ullam officiis, odio,
+      alias veritatis illum saepe quidem. Libero sapiente cumque eius debitis
+      eveniet adipisci ut! Excepturi sequi, commodi sapiente ab, quos vitae quas
+      et quod tempore accusantium illo ea voluptatem est accusamus. Facilis at
+      sapiente impedit saepe quo minima autem recusandae incidunt consequuntur
+      aspernatur, sequi quae hic beatae eaque fugit aliquid neque excepturi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, delectus
+      totam enim sint omnis possimus corporis a ducimus exercitationem eos
+      mollitia distinctio veniam optio sunt debitis magnam facere? Veniam porro
+      voluptatum perspiciatis harum aut minus at itaque consectetur vitae
+      veritatis. Voluptatibus amet eaque dicta nisi quis ullam officiis, odio,
+      alias veritatis illum saepe quidem. Libero sapiente cumque eius debitis
+      eveniet adipisci ut! Excepturi sequi, commodi sapiente ab, quos vitae quas
+      et quod tempore accusantium illo ea voluptatem est accusamus. Facilis at
+      sapiente impedit saepe quo minima autem recusandae incidunt consequuntur
+      aspernatur, sequi quae hic beatae eaque fugit aliquid neque excepturi.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
